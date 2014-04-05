1b28d312399a796f4630b687767cf9478e06340c
rachelslaybaugh <r.slaybaugh@gmail.com>
2014-04-05 16:48:13 -0700

    added a readme and an example input file.

---
+++ ExampleInput.py
@@ -0,0 +1,23 @@
+#!/Vendors/anaconda/bin/python
+
+import EstNumJellyBeans as jelly
+
+# Get a jelly bean estimator
+estimator = jelly.NumJellyEstimator()
+
+# Set the fraction of land being used for sugar, the world population,
+# and the fraction of the population that loves pink.
+land_frac = 0.1
+ppl = 7e9
+pink_frac = 0.18
+estimator.set_land_frac_for_sugar(land_frac)
+estimator.set_world_pop(ppl)
+estimator.set_frac_ppl_loving_pink(pink_frac)
+
+# Get the estimate of the number of jelly beans in the world,
+# and report the result given the input.
+est = estimator.compute_Njelly_est()
+
+print "\nIt is estimated that when\n", land_frac*100, "percent\nof the land is"+\
+      " used for sugar and\n", pink_frac*100, "percent of", int(ppl), \
+      "people\nLOVE pink, then there are\n", est, "\nJelly Beans in the world."
---
+++ readme.md
@@ -0,0 +1,11 @@
+Jelly Bean Code
+========
+
+The Jelly Bean Code was originally created as a Software Carpentry
+Instructor final project. 
+
+It currently contains one class that estimates the number of jelly 
+beans in the world based on a few variables.
+
+An example of how to use the code is in ExampleInput.py
+
